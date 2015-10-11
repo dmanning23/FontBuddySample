32168ac2e5bbd55ae6d6c8ecdf8baf23df28d600
Dan Manning <dmanning23@hotmail.com>
2015-10-11 13:59:54 -0500

    update proj files

--- Windows/Program.cs
+++ Windows/Program.cs
@@ -4,16 +4,16 @@ namespace FontBuddySample
 {
 	static class Program
 	{
-		private static Game1 game;
-
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		static void Main ()
 		{
-			game = new Game1 ();
-			game.Run ();
+			using (var game = new Game1())
+			{
+				game.Run();
+			}
 		}
 	}
 }
